c1c4a70196ea0c697135cfd104d8dcf82c02dc5a
sungbum <sungbum@aqara.kr>
2022-10-28 10:20:36 +0900

    set: amplify 초기설정

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "NoteAPP": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
